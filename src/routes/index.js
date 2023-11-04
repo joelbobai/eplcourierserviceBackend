d07c83eb7b6e208f5694ebd917047578fe918bbf
joelbobai <90658305+joelbobai@users.noreply.github.com>
2023-11-04 21:54:44 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const UserTrackingId = require("./../domains/userTrackingId");
+const TrackingItems = require("./../domains/trackingItems");
 
 router.use("/user_tracking_Id", UserTrackingId);
+router.use("/item_tracking_Id", TrackingItems);
 
 module.exports = router;
