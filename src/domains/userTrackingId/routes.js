63862eb714715f1d5dd04c0e7684526ed26b5c81
joelbobai <90658305+joelbobai@users.noreply.github.com>
2023-11-06 16:01:08 +0100

    Update routes.js

--- src/domains/userTrackingId/routes.js
+++ src/domains/userTrackingId/routes.js
@@ -20,7 +20,62 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.post("/tracking/create", async (req, res) => {
+  try {
+    let {
+      address,
+      discount,
+      delivery,
+      tax,
+      total,
+      trackingId,
+      orderDate,
+      estimatedDelivery,
+    } = req.body;
 
+    // id = id.trim();
+    const newUserTrackingId = new UserTrackingId({
+      trackingId: trackingId,
+      orderDate: orderDate,
+      estimatedDelivery: estimatedDelivery,
+      locationOne: {
+        location: req.body.locationOne.location,
+        isThere: req.body.locationOne.isThere === "true" ? true : false,
+        date: req.body.locationOne.date,
+      },
+      locationTwo: {
+        location: req.body.locationTwo.location,
+        isThere: req.body.locationTwo.isThere === "true" ? true : false,
+        date: req.body.locationTwo.date,
+      },
+      locationThree: {
+        location: req.body.locationThree.location,
+        isThere: req.body.locationThree.isThere === "true" ? true : false,
+        date: req.body.locationThree.date,
+      },
+      locationFour: {
+        location: req.body.locationFour.location,
+        isThere: req.body.locationFour.isThere === "true" ? true : false,
+        date: req.body.locationFour.date,
+      },
+      locationFive: {
+        location: req.body.locationFive.location,
+        isThere: req.body.locationFive.isThere === "true" ? true : false,
+        date: req.body.locationFive.date,
+      },
+      address: address,
+      discount: discount,
+      delivery: delivery,
+      tax: tax,
+      total: total,
+    });
+    // save user
+    const createdUser = await newUserTrackingId.save();
+    return res.status(200).json(createdUser);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 
 router.get("/tracking/get_all", async (req, res) => {
   try {
