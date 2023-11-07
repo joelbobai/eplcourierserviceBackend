e0e79504b3bf57a368f529939f4be7beaec98f56
joelbobai <90658305+joelbobai@users.noreply.github.com>
2023-11-07 02:45:23 +0100

    Update routes.js

--- src/domains/userTrackingId/routes.js
+++ src/domains/userTrackingId/routes.js
@@ -85,6 +85,71 @@ router.get("/tracking/get_all", async (req, res) => {
     res.status(400).send(error.message);
   }
 });
+router.post("/tracking/update", async (req, res) => {
+  try {
+    let {
+      address,
+      discount,
+      delivery,
+      tax,
+      total,
+      trackingId,
+      orderDate,
+      estimatedDelivery,
+    } = req.body;
+    const updateUsers = await UserTrackingId.findByIdAndUpdate(
+      { _id: req.body._id },
+      {
+        trackingId: trackingId,
+        orderDate: orderDate,
+        estimatedDelivery: estimatedDelivery,
+        locationOne: {
+          location: req.body.locationOne.location,
+          isThere: req.body.locationOne.isThere === "true" ? true : false,
+          date: req.body.locationOne.date,
+        },
+        locationTwo: {
+          location: req.body.locationTwo.location,
+          isThere: req.body.locationTwo.isThere === "true" ? true : false,
+          date: req.body.locationTwo.date,
+        },
+        locationThree: {
+          location: req.body.locationThree.location,
+          isThere: req.body.locationThree.isThere === "true" ? true : false,
+          date: req.body.locationThree.date,
+        },
+        locationFour: {
+          location: req.body.locationFour.location,
+          isThere: req.body.locationFour.isThere === "true" ? true : false,
+          date: req.body.locationFour.date,
+        },
+        locationFive: {
+          location: req.body.locationFive.location,
+          isThere: req.body.locationFive.isThere === "true" ? true : false,
+          date: req.body.locationFive.date,
+        },
+        address: address,
+        discount: discount,
+        delivery: delivery,
+        tax: tax,
+        total: total,
+      }
+    );
+    // save user
+    const updatedUsers = await updateUsers.save();
+    return res.status(200).json(updatedUsers);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+router.get("/tracking/delete", async (req, res) => {
+  try {
+    const deleteUsers = await UserTrackingId.findByIdAndDelete({});
+    return res.status(200).json(deleteUsers);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 
 
 module.exports = router;
